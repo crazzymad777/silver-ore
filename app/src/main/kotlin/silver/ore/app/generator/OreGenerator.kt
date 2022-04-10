d0e0d00f9acc5d0e2752141aa4d9d89bd0b53ebe
crazzymad777 <aerobus.778@gmail.com>
2022-04-10 19:00:28 +0300

    first ore generator issue #3 issue #4

--- app/src/main/kotlin/silver/ore/app/generator/OreGenerator.kt
+++ app/src/main/kotlin/silver/ore/app/generator/OreGenerator.kt
@@ -4,22 +4,45 @@ import silver.ore.app.AbstractGenerator
 import silver.ore.app.Cube
 import silver.ore.app.Material
 import silver.ore.app.game.Ore
+import java.util.*
 import kotlin.Int.Companion.MAX_VALUE
 import kotlin.math.pow
 import kotlin.random.Random
+
+data class Cluster(val x: Int, val y: Int, val z: Int, val material: Material, val radius: Int)
 
 class OreGenerator(random: Random = Random(0)) : AbstractGenerator(random) {
     private val offsetX = random.nextInt(until = MAX_VALUE) + 1
     private val offsetY = random.nextInt(until = MAX_VALUE) + 1
     private val offsetZ = random.nextInt(until = MAX_VALUE) + 1
+    // gold (1-5), silver (1-15), tin (1-10), copper (1-15), iron (1-20)
+    private val oreClusters: Vector<Cluster> = Vector()
+    private fun produce(): Int {
+        return random.nextInt(0, 256)
+    }
+    init {
+        for (i in 1..5)
+            oreClusters.addElement(Cluster(produce(), produce(), random.nextInt(0, 128), Material.GOLD, random.nextInt(5, 10)))
+        for (i in 1..15)
+            oreClusters.addElement(Cluster(produce(), produce(), random.nextInt(0, 128), Material.SILVER, random.nextInt(5, 10)))
+        for (i in 1..10)
+            oreClusters.addElement(Cluster(produce(), produce(), random.nextInt(0, 128), Material.TIN, random.nextInt(5, 10)))
+        for (i in 1..15)
+            oreClusters.addElement(Cluster(produce(), produce(), random.nextInt(0, 128), Material.COPPER, random.nextInt(5, 10)))
+        for (i in 1..20)
+            oreClusters.addElement(Cluster(produce(), produce(), random.nextInt(0, 128), Material.IRON, random.nextInt(5, 10)))
+    }
     override fun getCube(x: Int, y: Int, z: Int): Cube? {
         if (z <= 124) {
             var ore: Ore? = null
             val newX = (x+offsetX)%256
             val newY = (y+offsetY)%256
             val newZ = (z+offsetZ)%256
-            if ((newX - x).toDouble().pow(2) +(newY-y).toDouble().pow(2)+(newZ-z).toDouble().pow(2) < 125) {
-                ore = Ore(Material.SILVER)
+            for (cluster in oreClusters) {
+                if ((newX - cluster.x).toDouble().pow(2) + (newY -  cluster.y).toDouble().pow(2) + (newZ -  cluster.z).toDouble().pow(2) < cluster.radius.toDouble().pow(2)) {
+                    ore = Ore(cluster.material)
+                    break
+                }
             }
             return Cube(Material.STONE, Material.STONE, ore = ore)
         }
