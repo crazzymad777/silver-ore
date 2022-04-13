18cb6e67312490fada684af33ea57f6101a68f0f
crazzymad777 <aerobus.778@gmail.com>
2022-04-13 16:55:47 +0300

    fix negative x & y

--- app/src/main/kotlin/silver/ore/app/utils/GlobalCubeCoordinates.kt
+++ app/src/main/kotlin/silver/ore/app/utils/GlobalCubeCoordinates.kt
@@ -4,24 +4,6 @@ package silver.ore.app.utils
 data class GlobalCubeCoordinates(val x: Int, val y: Int, val z: Int)
 {
     fun getChunkCoordinates(): WorldChunkCoordinates {
-//        var x = this.x
-//        if (x < 0) {
-//            x = 16 - x
-//            x *= -1
-//        }
-//        var y = this.y
-//        if (y < 0) {
-//            y = 16 - y
-//            y *= -1
-//        }
-        var x = this.x
-        if (x < 0) {
-            x -= 16
-        }
-        var y = this.y
-        if (y < 0) {
-            y -= 16
-        }
         return WorldChunkCoordinates(x/16, y/16, z/16)
     }
 }
