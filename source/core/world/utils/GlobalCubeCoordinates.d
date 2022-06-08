ea64eb89cdf6da1d1dbbeef411810f90d804cccb
crazzymad777 <aerobus.778@gmail.com>
2022-06-08 19:43:50 +0300

    migration to dlang

--- source/core/world/utils/GlobalCubeCoordinates.d
+++ source/core/world/utils/GlobalCubeCoordinates.d
@@ -2,4 +2,17 @@ module core.world.utils;
 
 struct GlobalCubeCoordinates {
   long x, y, z;
+  static GlobalCubeCoordinates fromInternalCoordinates(ulong x, ulong y, ulong z) {
+      return GlobalCubeCoordinates(x, y, z);
+  }
+}
+
+/// test fromInternalCoordinates
+private unittest {
+  ulong x = 12;
+  ulong y = -12;
+  ulong z = 13;
+
+  auto actual = GlobalCubeCoordinates.fromInternalCoordinates((-1*x), (-1*y), (-1*z));
+  assert(GlobalCubeCoordinates(-1*x, -1*y, -1*z) == actual);
 }
