7e437bcb8df5d0cc6026e1c652affbcff13d0780
crazzymad777 <aerobus.778@gmail.com>
2022-06-25 20:19:22 +0300

    add PaladinController

---
+++ source/custom/paladin/PaladinController.d
@@ -0,0 +1,101 @@
+module custom.paladin.PaladinController;
+
+import custom.paladin.world.TextState;
+import custom.paladin.core.Stats;
+import custom.paladin.core.Game;
+import core.game.Mob;
+
+import core.world.utils.GlobalCubeCoordinates;
+import core.world.Cube;
+
+interface IPaladinController {
+  static PaladinControllerImpl getImplementation() {
+    return new PaladinControllerImpl();
+  }
+
+  void process();
+  void resetGame();
+  void move(int x = 0, int y = 0);
+  void attack();
+  void follow();
+  Mob getPaladin();
+  Mob[] getMobs();
+  long count();
+  long cubesLoaded();
+  Stats stats();
+  TextState textState();
+
+  bool endCondition();
+  long getTick();
+  bool checkVisible(GlobalCubeCoordinates a, GlobalCubeCoordinates b);
+  Cube getCube(GlobalCubeCoordinates coors);
+}
+
+class PaladinControllerImpl : IPaladinController {
+  private Game game;
+  this() {
+    this.game = new Game();
+  }
+
+  void process() {
+    game.process();
+  }
+
+  void resetGame() {
+    if (game.EndCondition()) {
+      game = new Game();
+    }
+  }
+
+  void move(int x, int y) {
+    game.getPaladin().move(x, y);
+  }
+
+  void attack() {
+    game.getPaladin().attack();
+  }
+
+  void follow() {
+    game.follow();
+  }
+
+  Mob getPaladin() {
+    return game.getPaladin();
+  }
+
+  Mob[] getMobs() {
+    return game.getMobs();
+  }
+
+  long count() {
+    return game.count;
+  }
+
+  long cubesLoaded() {
+    return game.world.cubesLoaded();
+  }
+
+  Stats stats() {
+    return game.stats;
+  }
+
+  TextState textState() {
+    return game.textState;
+  }
+
+  bool endCondition() {
+    return game.EndCondition();
+  }
+
+  long getTick() {
+    return game.getTick();
+  }
+
+  bool checkVisible(GlobalCubeCoordinates a, GlobalCubeCoordinates b) {
+    return game.world.checkVisible(a, b);
+  }
+
+  Cube getCube(GlobalCubeCoordinates coors) {
+    return game.world.getCube(coors);
+  }
+}
--- source/custom/paladin/terminal/PaladinComponent.d
+++ source/custom/paladin/terminal/PaladinComponent.d
@@ -12,11 +12,10 @@ import std.format;
 import std.conv;
 
 import core.world.utils.GlobalCubeCoordinates;
-import custom.paladin.core.Game;
-import core.world.WorldConfig;
 
 class PaladinComponent : AbstractComponent {
-    private Game game;
+    import custom.paladin.PaladinController;
+    private IPaladinController controller;
     private auto updated = true;
     private auto exited = false;
     private ITerminal terminal;
@@ -25,7 +24,7 @@ class PaladinComponent : AbstractComponent {
       import terminal.Settings: enableNcTerminal;
       ITerminal terminal = ITerminal.getDefaultTerminal(!enableNcTerminal, this);
 
-      this.game = new Game();
+      this.controller = IPaladinController.getImplementation();
       this.terminal = terminal;
       /* this.coors = world.getDefaultCoordinates(); */
     }
@@ -45,7 +44,7 @@ class PaladinComponent : AbstractComponent {
     }
 
     override void process() {
-      game.process();
+      controller.process();
     }
 
     bool showTicks = false;
@@ -55,34 +54,33 @@ class PaladinComponent : AbstractComponent {
 
       bool prepend = false;
       if (c == 'r') {
-        if (game.EndCondition()) {
-          game = new Game();
-        }
+        controller.resetGame();
       } if (c == 'q') {
         /* game.apocalypse(); */
         exited = true;
       } else if (c == 'w') {
-        game.getPaladin().move(0, -1);
+        controller.move(0, -1);
         prepend = true;
       } else if (c == 's') {
-        game.getPaladin().move(0, 1);
+        controller.move(0, 1);
         prepend = true;
       } else if (c == 'a') {
-        game.getPaladin().move(-1);
+        controller.move(-1);
         prepend = true;
       } else if (c == 'd') {
-        game.getPaladin().move(1);
+        controller.move(1);
         prepend = true;
       } else if (c == '0') {
         showTicks = !showTicks;
         prepend = true;
       } else if (c == 'f') {
-        game.follow();
+        controller.follow();
         prepend = true;
       } else if (c == 'p') {
-        game.getPaladin().attack();
+        controller.attack();
         prepend = true;
       }
+
       if (prepend || key.getKeycode() == 0) {
         import std.conv: to;
         if (key.getKeycode() == 0) {
@@ -109,7 +107,7 @@ class PaladinComponent : AbstractComponent {
         // fill display matrix
         int width = terminal.width()*1/2;
         import core.game.animals.Animal;
-        Animal hero = cast(Animal) game.getPaladin();
+        Animal hero = cast(Animal) controller.getPaladin();
         auto coors = hero.position;
 
         import core.game.Mob;
@@ -117,9 +115,9 @@ class PaladinComponent : AbstractComponent {
         import core.game.animals.Lion;
         import core.game.monsters.Monster;
 
-        auto mobs = game.getMobs();
+        auto mobs = controller.getMobs();
 
-        if (showTicks) terminal.puts(0, width + 1, format("World tick: %d / Cubes loaded: %d", game.count, game.world.cubesLoaded()));
+        if (showTicks) terminal.puts(0, width + 1, format("World tick: %d / Cubes loaded: %d", controller.count(), controller.cubesLoaded()));
 
         import std.utf;
         string t;
@@ -130,7 +128,7 @@ class PaladinComponent : AbstractComponent {
         }
         terminal.puts(0, width + 1, format("%s", t));
         int k = 0;
-        foreach(entry; game.stats.entries.byKeyValue()) {
+        foreach(entry; controller.stats().entries.byKeyValue()) {
           terminal.puts(2 + k*5, width + width/2 + 1,
                         format("%s", entry.key));
           terminal.puts(3 + k*5, width + width/2 + 1,
@@ -144,10 +142,12 @@ class PaladinComponent : AbstractComponent {
           k++;
         }
 
-        auto frens = game.getMobs();
+        auto frens = mobs;
         for (int i = 0; i < frens.length; i++) {
           if (frens[i] !is null) {
           auto fren = frens[i];
+          auto state = controller.textState();
+
           if (fren == hero) {
             terminal.puts(2 + i*4, width + 1,
                           format("You're %s", hero.getName()));
@@ -157,13 +157,13 @@ class PaladinComponent : AbstractComponent {
           }
 
           terminal.puts(3 + i*4, width + 1,
-                           format("+ %s", game.textState.getStamina(fren.stamina, fren.maxStamina)),
-                           game.textState.getStaminaColor(fren.stamina, fren.maxStamina)
+                           format("+ %s", state.getStamina(fren.stamina, fren.maxStamina)),
+                           state.getStaminaColor(fren.stamina, fren.maxStamina)
                            );
 
           terminal.puts(4 + i*4, width + 1,
-                           format("+ %s (%d)", game.textState.getHealth(fren.hitpoints, fren.maxHitpoints), fren.hitpoints),
-                           game.textState.getHealthColor(fren.hitpoints, fren.maxHitpoints)
+                           format("+ %s (%d)", state.getHealth(fren.hitpoints, fren.maxHitpoints), fren.hitpoints),
+                           state.getHealthColor(fren.hitpoints, fren.maxHitpoints)
                            );
 
            if (cast(Animal) fren) {
@@ -184,7 +184,7 @@ class PaladinComponent : AbstractComponent {
             auto w = 'x';
             auto color = TerminalColor.WHITE;
             auto lookAt = GlobalCubeCoordinates(coors.x + i, coors.y + j, coors.z);
-            if (game.world.checkVisible(hero.position, lookAt) || !hero.isAlive() || game.EndCondition()) {
+            if (controller.checkVisible(hero.position, lookAt) || !hero.isAlive() || controller.endCondition()) {
               Mob entity;
               foreach (mob; mobs) {
                 if (mob.position == lookAt) {
@@ -194,7 +194,7 @@ class PaladinComponent : AbstractComponent {
               }
 
               if (entity is null) {
-                auto cube = game.world.getCube(lookAt);
+                auto cube = controller.getCube(lookAt);
                 auto glyph = new Glyph(cube);
                 w = glyph.display();
                 color = glyph.foreground;
@@ -212,7 +212,7 @@ class PaladinComponent : AbstractComponent {
                 }
 
                 if (Animal animal = cast(Animal) entity) {
-                  if (animal.attackedTick + 5 > game.getTick()) {
+                  if (animal.attackedTick + 5 > controller.getTick()) {
                     color = TerminalColor.MAGENTA;
                   }
                 }
