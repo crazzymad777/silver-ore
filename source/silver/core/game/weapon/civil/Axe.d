55c0909929453275fd11a7fef0333e80669a6790
crazzymad777 <aerobus.778@gmail.com>
2022-08-21 13:03:43 +0300

    add Axe (civil weapon)

---
+++ source/silver/core/game/weapon/civil/Axe.d
@@ -0,0 +1,11 @@
+module silver.core.game.weapon.civil.Axe;
+
+import silver.core.game.Weapon;
+
+class Axe : Weapon {
+  // slashing
+  this() {
+    this.name = "axe";
+    this.damageDice = 4;
+  }
+}
